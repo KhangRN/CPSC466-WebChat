99253a15480bbf4674ce8448a25a6db0db25f4b7
Jordan Pourazari <jordan@vantageled.com>
2017-02-27 19:42:24 -0800

    Initial setup finished

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use AppBundle\Entity\Users;
+use Symfony\Component\VarDumper\VarDumper;
+
+class DefaultController extends Controller
+{   
+    /**
+     * @Route("/register", name="registerPage")
+     */
+    public function registerPage(Request $request){
+        return $this->render('register.html.twig');
+    }
+    
+    /**
+     * @Route("/login", name="loginPage")
+     */
+    public function loginPage(Request $request){
+        return $this->render('login.html.twig');
+    }
+    
+    /**
+     * @Route("/private", name="privatePage")
+     */
+    public function privatePage(Request $request){
+        $session = $request->getSession();
+        if(!$session->has('uid')){
+            return $this->redirectToRoute('loginPage');
+        }
+        else{
+            $uid = $session->get('uid');
+            return $this->render('private.html.twig', array('uid' => $uid));
+        }   
+    }
+    
+    /**
+     * @Route("/actions/login", name="loginAction")
+     */
+    public function loginAction(Request $request){
+        $em = $this->getDoctrine()->getManager();
+        
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+        
+        //check to ensure we have both require POST parameters
+        if(empty($email) || empty($password)){
+            return $this->render('login.html.twig', array('error' => "Invalid email or password"));
+        }
+        else{
+            $user = $em->getRepository('AppBundle:Users')->findOneBy(array('username' => $email));
+            if(empty($user)){
+                return $this->render('login.html.twig', array('error' => "Invalid email or password"));
+            }
+            //TO-DO: implement session timeout after ~5 minutes
+            if($password == $user->getPassword()){
+                $uid = $user->getUserId();
+                $session = $request->getSession();
+                $session->set('uid', $uid);
+                return $this->redirectToRoute('privatePage');
+            }
+            else{
+                return $this->render('login.html.twig', array('error' => "Invalid email or password"));
+            }
+        }
+    }
+    
+    /**
+     * @Route("/actions/register", name="registerAction")
+     */
+    public function registerAction(Request $request){
+        $email = $request->request->get('new-email');
+        $password = $request->request->get('new-password');
+        
+        if(empty($email) || empty($password)){
+            return $this->render('register.html.twig', array('error' => "Missing email or password"));
+        }
+        else{
+            $em = $this->getDoctrine()->getManager('default');
+            $u = new Users();
+            $u->setUsername($email);
+            $u->setPassword($password);
+            $u->setFirstname('New User');
+            $em->persist($u);
+            $em->flush();
+            $uid = $u->getUserId();
+            $session = $request->getSession();
+            $session->set('uid', $uid);
+            return $this->redirectToRoute('privatePage');
+        }
+    }
+}
---
+++ src/AppBundle/Entity/Users.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Users
+ *
+ * @ORM\Table(name="users")
+ * @ORM\Entity
+ */
+class Users
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="userid", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $userid;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255, nullable=false)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255, nullable=false)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=255, nullable=false)
+     */
+    private $firstname;
+
+
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return Users
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return Users
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return Users
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Get userid
+     *
+     * @return integer
+     */
+    public function getUserid()
+    {
+        return $this->userid;
+    }
+}
